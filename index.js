46300848c303e5d352903a6bd7f605554bc3af7b
catxrine <catxrine@gmail.com>
2024-07-31 23:21:22 +0300

    made it a singleton

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-import FolderDB from './FolderDB/index.js';
+import FolderDB from "./FolderDB/index.js";
 
 const db = new FolderDB();
+console.log("==============================================");
 
-console.log('==============================================');
-
-const result = await db.get('');
-console.log('this is ', result);
-// db.get('users');
+const result = await db.get("");
+console.log("this is ", result);
+// db.get("users");
 // db.get('users.posts.1234.author');
 
-console.log('==============================================');
+console.log("==============================================");
