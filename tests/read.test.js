2bf8b3f68f7045b49a5dba3ca5117a87d1983e0d
catxrine <catxrin@gmail.com>
2024-09-05 09:41:41 +0300

    used path.resolve for consistent behavior in the [FILES] and [VALUE] tests

--- tests/read.test.js
+++ tests/read.test.js
@@ -1,120 +1,122 @@
-import { describe, expect, test } from 'bun:test';
-import * as fs from 'fs';
+import { describe, expect, test } from "bun:test";
+import * as fs from "fs";
+import FolderDB from "../FolderDB/index.js";
+import { ValueType } from "../FolderDB/utils/enums.js";
+import path from "path";
 
-import FolderDB from '../FolderDB/index.js';
-import { ValueType } from '../FolderDB/utils/enums.js';
+const db = new FolderDB({ dbPath: "./db", mergeInstances: true });
 
-const db = new FolderDB({ dbPath: './db', mergeInstances: true });
-
-describe('[FOLDERS]', () => {
-  test('Wrong db path', async () => {
+describe("[FOLDERS]", () => {
+  test("Wrong db path", async () => {
     const errorDB = db._clone();
-    errorDB.dbPath = './nothing';
+    errorDB.dbPath = "./nothing";
 
     expect(() => {
-      errorDB.get('nothing');
-    }).toThrow('Invalid directory path: ./nothing');
+      errorDB.get("nothing");
+    }).toThrow("Invalid directory path: ./nothing");
   });
 
-  test('Wrong parameters', async () => {
+  test("Wrong parameters", async () => {
     expect(() => {
       db.get();
-    }).toThrow('Value must be string');
+    }).toThrow("Value must be string");
   });
 
-  test('Empty pointers', async () => {
-    const result = db.get('');
-    const dir = fs.readdirSync('./db');
+  test("Empty pointers", async () => {
+    const result = db.get("");
+    const dir = fs.readdirSync("./db");
 
     expect(result.data).toEqual(dir);
   });
 
-  test('Folder navigation', async () => {
-    const result = db.get('users');
-    const dir = fs.readdirSync('./db/users');
+  test("Folder navigation", async () => {
+    const result = db.get("users");
+    const dir = fs.readdirSync("./db/users");
 
     expect(result.data).toEqual(dir);
     expect(result.valueType).toEqual(ValueType.DIRECTORY);
   });
 
-  test('Wrong directory', async () => {
+  test("Wrong directory", async () => {
     expect(() => {
-      db.get('nothing');
-    }).toThrow('Error reading file');
+      db.get("nothing");
+    }).toThrow("Error reading file");
   });
 });
 
-describe('[FILES]', () => {
-  test('Navigating to a file', async () => {
-    const result = db.get('users.posts.1234');
-    const data = fs.readFileSync('./db/users/posts/1234.json', 'UTF-8');
+describe("[FILES]", () => {
+  test("Navigating to a file", async () => {
+    const result = db.get("users.posts.1234");
+    const directory = path.resolve("db/users/posts/1234.json");
+    const data = fs.readFileSync(directory, "UTF-8");
 
     expect(result.data).toEqual(JSON.parse(data));
     expect(result.valueType).toEqual(ValueType.FILE);
-    expect(result.targetFile).toMatch('db/users/posts/1234.json');
+    expect(result.targetFile).toMatch(directory);
   });
 
-  test('Wrong file', async () => {
+  test("Wrong file", async () => {
     expect(() => {
-      db.get('users.posts.nothing');
-    }).toThrow('Error reading file');
+      db.get("users.posts.nothing");
+    }).toThrow("Error reading file");
   });
 });
 
-describe('[VALUES]', () => {
-  test('Pointers within a file', async () => {
-    const result = db.get('users.posts.1234.title');
-    const data = fs.readFileSync('./db/users/posts/1234.json', 'UTF-8');
+describe("[VALUES]", () => {
+  test("Pointers within a file", async () => {
+    const result = db.get("users.posts.1234.title");
+    const directory = path.resolve("db/users/posts/1234.json");
+    const data = fs.readFileSync(directory, "UTF-8");
 
     expect(result.data).toEqual(JSON.parse(data).title);
     expect(result.valueType).toEqual(ValueType.VALUE);
-    expect(result.targetFile).toMatch('db/users/posts/1234.json');
+    expect(result.targetFile).toMatch(directory);
   });
 
-  test('Navigating arrays', async () => {
-    const result = db.get('users.posts.1234.links.0.name');
-    const data = fs.readFileSync('./db/users/posts/1234.json', 'UTF-8');
+  test("Navigating arrays", async () => {
+    const result = db.get("users.posts.1234.links.0.name");
+    const data = fs.readFileSync("./db/users/posts/1234.json", "UTF-8");
 
     expect(result.data).toEqual(JSON.parse(data).links[0].name);
   });
 
-  test('Wrong value', async () => {
+  test("Wrong value", async () => {
     expect(() => {
-      db.get('users.posts.1234.nothing');
-    }).toThrow('Path not found: nothing');
+      db.get("users.posts.1234.nothing");
+    }).toThrow("Path not found: nothing");
   });
 });
 
-describe('[TREE]', () => {
+describe("[TREE]", () => {
   // TODO
 });
 
-describe('[BACK]', () => {
-  test('Navigating backwards', async () => {
-    const data = fs.readFileSync('./db/users/posts/1234.json', 'UTF-8');
+describe("[BACK]", () => {
+  test("Navigating backwards", async () => {
+    const data = fs.readFileSync("./db/users/posts/1234.json", "UTF-8");
 
-    const result = db.get('users.posts.1234.author.name');
+    const result = db.get("users.posts.1234.author.name");
     expect(result.data).toEqual(JSON.parse(data).author.name);
 
     result.back();
     expect(result.data).toEqual(JSON.parse(data).author);
 
     result.back(2);
-    const dirData = fs.readdirSync('./db/users/posts');
+    const dirData = fs.readdirSync("./db/users/posts");
     expect(result.data).toEqual(dirData);
   });
 
-  test('Wrong back value', async () => {
+  test("Wrong back value", async () => {
     expect(() => {
-      db.get('users.posts.1234').back('1234');
-    }).toThrow('Steps must be a positive number');
+      db.get("users.posts.1234").back("1234");
+    }).toThrow("Steps must be a positive number");
   });
 });
 
-describe('[OTHER]', () => {
-  test('Chaining methods', async () => {
-    const result = db.get('users.posts').get('1234').get('title');
-    const data = fs.readFileSync('./db/users/posts/1234.json', 'UTF-8');
+describe("[OTHER]", () => {
+  test("Chaining methods", async () => {
+    const result = db.get("users.posts").get("1234").get("title");
+    const data = fs.readFileSync("./db/users/posts/1234.json", "UTF-8");
 
     expect(result.data).toEqual(JSON.parse(data).title);
   });
