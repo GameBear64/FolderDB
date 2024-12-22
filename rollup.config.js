94656f436f2b0ab6eb0dd7df357df1d738416f4f
GamBar <vlpetrov02@gmail.com>
2024-12-22 03:36:41 +0200

    minor adjustments, tested out minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,31 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+// import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: 'dist/index.cjs',
+      file: 'dist/index.full.cjs',
       format: 'cjs',
       exports: 'named',
     },
     {
-      file: 'dist/index.js',
+      file: 'dist/index.full.js',
       format: 'es',
     },
+    // Would be cool but for a backend package does it matter?
+    // {
+    //   file: 'dist/index.cjs',
+    //   format: 'cjs',
+    //   exports: 'named',
+    //   plugins: [terser({ keep_fnames: true })],
+    // },
+    // {
+    //   file: 'dist/index.js',
+    //   format: 'es',
+    //   plugins: [terser({ keep_fnames: true })],
+    // },
   ],
   plugins: [resolve(), commonjs()],
 };
